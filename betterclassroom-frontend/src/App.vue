09ee64796dd73b16c6f63c73b77608b1d23d6713
Nielshen <Niels.hensler@gmail.com>
2024-05-02 21:03:16 +0200

    Add backend service to ingress

--- betterclassroom-frontend/src/App.vue
+++ betterclassroom-frontend/src/App.vue
@@ -6,8 +6,8 @@ const students = ref([])
 
 const fetchStudents = async () => {
   try {
-    // const response = await axios.get('http://localhost:5000/students')
-    const response = await axios.get('http://localhost:5000/students')
+    // const response = await axios.get('http://better-classroom.com:8088/api/students')
+    const response = await axios.get('http://better-classroom.com:8088/api/students')
     console.log("Daten empfangen:", response.data);
     students.value = response.data
   } catch (error) {
