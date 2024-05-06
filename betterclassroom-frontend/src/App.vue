b769e63671d40b1990519c5c6380ed000c0a0ecb
Sebastian Kaeser <s.kaeser@blein.io>
2024-05-06 19:06:25 +0200

    erstes Dashboard Design

--- betterclassroom-frontend/src/App.vue
+++ betterclassroom-frontend/src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import axios from 'axios'
 import { ref } from 'vue'
+import Navbar from './components/Navbar.vue'
 
 const students = ref([])
 
 const fetchStudents = async () => {
   try {
-    // const response = await axios.get('http://better-classroom.com:8088/api/students')
     const response = await axios.get('http://better-classroom.com:8088/api/students')
     console.log("Daten empfangen:", response.data);
     students.value = response.data
@@ -17,18 +17,7 @@ const fetchStudents = async () => {
 </script>
 
 <template>
-  <button @click="fetchStudents">Get Students</button>
-  <ul>
-    <!-- Loop -->
-    <li v-for="student in students" :key="student.id">{{ student.name }}</li>
-  </ul>
+  <Navbar />
+  <router-view></router-view>
 </template>
 
-<style scoped>
-button {
-  margin: 20px;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-}
-</style>
---
+++ betterclassroom-frontend/src/components/DashboardPerson.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { defineProps, computed } from 'vue'
+const props = defineProps(
+    {
+        name: String,
+        seat: String,
+        maxTasks: Number,
+        finishedTasks: Number
+    }
+)
+
+const progress = computed(() => {
+    return (props.finishedTasks / props.maxTasks) * 100
+})
+</script>
+<template>
+<!-- 30 -> Base / 8 -> Anzahl Karten -->
+<!-- 20 -> Base / 5 -> Anzahl Karten -->
+  <div class="card w-[15rem] h-[8rem] overflow-hidden bg-primary text-primary-content my-2 mr-3">
+    <div class="p-3">
+      <div class="flex flex-row justify-between">
+        <div class="mt-4">
+          <h2 class="card-title text-lg">{{ seat }}</h2>
+          <h2 class="card-title">{{ name }}</h2>
+        </div>
+        <div>
+          <div class="radial-progress" :style="`--value: ${progress}`" role="progressbar">{{ progress }}%</div>
+          <!--<h2>{{ finishedTasks }} / {{ maxTasks }}</h2>-->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
