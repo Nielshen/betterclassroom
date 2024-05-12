bf07e3ddfd82b98088fe9ab163843f09868df94e
ZdravkoMat <zd.matic96@gmail.com>
2024-05-12 10:38:47 +0200

    first concept coursesview, smaller changes to dashboard

--- betterclassroom-frontend/src/components/DashboardPerson.vue
+++ betterclassroom-frontend/src/components/DashboardPerson.vue
@@ -13,22 +13,9 @@ const progress = computed(() => {
     return (props.finishedTasks / props.maxTasks) * 100
 })
 </script>
-<template>
-<!-- 30 -> Base / 8 -> Anzahl Karten -->
-<!-- 20 -> Base / 5 -> Anzahl Karten -->
-  <div class="card w-[15rem] h-[8rem] overflow-hidden bg-primary text-primary-content my-2 mr-3">
-    <div class="p-3">
-      <div class="flex flex-row justify-between">
-        <div class="mt-4">
-          <h2 class="card-title text-lg">{{ seat }}</h2>
-          <h2 class="card-title">{{ name }}</h2>
-        </div>
-        <div>
-          <div class="radial-progress" :style="`--value: ${progress}`" role="progressbar">{{ progress }}%</div>
-          <!--<h2>{{ finishedTasks }} / {{ maxTasks }}</h2>-->
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
 
+<template>
+    <div>
+        <div class="radial-progress" :style="`--value: ${progress}`" role="progressbar">{{ finishedTasks }} / {{ maxTasks }}</div>
+    </div>
+</template>
---
+++ betterclassroom-frontend/src/components/Sidebar.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+const data = [
+    {
+        name: "Software Qualitätssicherung",
+        raum: "O-201",
+        count: 25
+    },
+    {
+        name: "Web-Technologien",
+        raum: "O-301",
+        count: 14
+    },
+]
+
+</script>
+
+<template>
+    <div class="drawer lg:drawer-open ">
+        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col items-center">
+            <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+            <router-view></router-view>
+        </div>
+        <div class="drawer-side">
+            <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+            <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                <li><a href="/courses">Kurse</a></li>
+                <li><a href="/tasks">Aufgaben</a></li>
+                <li><a href="/dashboard">Studenten</a></li>
+            </ul>
+
+        </div>
+    </div>
+</template>
