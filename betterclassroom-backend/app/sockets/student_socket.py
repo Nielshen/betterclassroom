74ef94143b73da85dc7c120495fe737c06f70320
Nielshen <Niels.hensler@gmail.com>
2024-06-01 19:09:04 +0200

    test websockets

--- betterclassroom-backend/app/sockets/student_socket.py
+++ betterclassroom-backend/app/sockets/student_socket.py
@@ -4,10 +4,13 @@ from flask_socketio.namespace import Namespace
 
 class StudentNamespace(Namespace):
     def on_connect(self):
-        emit("response", {"data": "Connection established test123"})
+        emit("response", {"data": "Connection established"})
 
     def on_disconnect(self):
-        pass
+        print("Client disconnected")
 
+    def on_message(self, message):
+        response_message = f"Received your message: {message}"
+        emit("response", {"data": response_message})
 
 student_ns = StudentNamespace("/student")
