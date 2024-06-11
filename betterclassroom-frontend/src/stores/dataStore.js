8c10edac3e142369c0acb6eb30303ef8b2b67b86
Sebastian Kaeser <s.kaeser@blein.io>
2024-06-11 13:53:27 +0200

    dynamicSidebar + mini user management

--- betterclassroom-frontend/src/stores/dataStore.js
+++ betterclassroom-frontend/src/stores/dataStore.js
@@ -8,7 +8,30 @@ export const useDataStore = defineStore('dataStore', () => {
 
     const dashboardData = ref([])
 
+    const saveStudent = ({ id, table }) => {
+        user.value = { id, table,  role: 'student' }
+        localStorage.setItem('user', JSON.stringify(user.value))
+    }
 
-    return { user, isStudent, isLoggedIn, dashboardData}
+    const initStudent = () => {
+        user.value = { role: 'student' }
+    }
+
+    const deleteStudent = () => {
+        user.value = {}
+        localStorage.removeItem('user')
+    }
+
+    const checkUser = () => {
+        const userString = localStorage.getItem('user')
+        if (userString) {
+            user.value = JSON.parse(userString)
+            return true
+        }
+        return false
+    }
+
+
+    return { user, isStudent, isLoggedIn, dashboardData ,saveStudent, deleteStudent, checkUser, initStudent}
 
 })
--- betterclassroom-frontend/src/utils/common.js
+++ betterclassroom-frontend/src/utils/common.js
@@ -3,6 +3,7 @@ const getApiUrl = (logging = false) => {
     const prodUrl = import.meta.env.VITE_API_PROD_URL_RAW
     const localClusterUrl = import.meta.env.VITE_API_CLUSTER_URL_RAW
     const testUrl = import.meta.env.VITE_API_TEST_URL_RAW
+    return prodUrl
     const hostname = window.location.hostname
     logging ? console.log({ prodUrl, testUrl, hostname}) : null
     if (hostname === 'localhost' || hostname.startsWith("127.")) {
