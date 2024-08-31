b08ba5d18f6c3b19c3a10de8079a5e12c1a4fb74
Sebastian Kaeser <s.kaeser@blein.io>
2024-08-31 16:13:37 +0200

    ProfessorLoginView & ProfessorChangePasswortView style

--- betterclassroom-frontend/src/views/ProfessorChangePasswordView.vue
+++ betterclassroom-frontend/src/views/ProfessorChangePasswordView.vue
@@ -13,76 +13,76 @@ const rawUrl = getApiUrl()
 const professorApiUrl = `http://${rawUrl}/api/professor`
 
 const register = async () => {
-    const data = {
-        email: email.value,
-        last_name: lastName.value,
-        first_name: firstName.value,
-        password: password1.value,
-        password2: password2.value
-    }
+  const data = {
+    email: email.value,
+    last_name: lastName.value,
+    first_name: firstName.value,
+    password: password1.value,
+    password2: password2.value
+  }
 
-    try {
-        //const response = await axios.post(`${professorApiUrl}/register`, data)
-        null
-    } catch(e) {
-
-        console.error(e)
-    
-    }
-
-    
+  try {
+    //const response = await axios.post(`${professorApiUrl}/register`, data)
+    null
+  } catch (e) {
+    console.error(e)
+  }
 }
-
 </script>
 <template>
-  <div class="flex flex-col h-full my-10 p-4 items-center justify-between border-4 border-black ">
-  <h1>Change Password</h1>
-    <label class="input input-bordered flex items-center gap-2 my-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        class="w-4 h-4 opacity-70"
-      >
-        <path
-          d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
-        />
-        <path
-          d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
-        />
-      </svg>
-      <input type="text" v-model="email" class="grow" placeholder="Email" />
-    </label>
-    <label class="input input-bordered flex items-center gap-2 my-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        class="w-4 h-4 opacity-70"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <input type="password" v-model="password1" class="grow" value="password" />
-    </label>
-    <label class="input input-bordered flex items-center gap-2 my-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        class="w-4 h-4 opacity-70"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <input type="password" v-model="password2" class="grow" value="password" />
-    </label>
-    <button @click="register"  class="btn btn-primary">Register</button>
+  <div class="flex flex-col items-center justify-center border-black">
+    <div
+      class="flex flex-col w-[400px] h-full my-10 p-8 justify-between border-2 shadow-2xl rounded-xl"
+    >
+      <h1 class="text-2xl">Better Classroom</h1>
+      <h1 class="text-xl font-bold mb-10">Neues Passwort erstellen</h1>
+      <label class="input input-bordered flex items-center gap-2 my-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          class="w-4 h-4 opacity-70"
+        >
+          <path
+            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
+          />
+          <path
+            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
+          />
+        </svg>
+        <input type="text" v-model="email" class="grow" placeholder="Email" />
+      </label>
+      <label class="input input-bordered flex items-center gap-2 my-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          class="w-4 h-4 opacity-70"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <input type="password" v-model="password1" class="grow" value="password" />
+      </label>
+      <label class="input input-bordered flex items-center gap-2 my-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          class="w-4 h-4 opacity-70"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <input type="password" v-model="password2" class="grow" value="password" />
+      </label>
+      <button @click="register" class="btn btn-primary mt-3">Passwort ändern</button>
+    </div>
   </div>
 </template>
--- betterclassroom-frontend/src/views/ProfessorLoginView.vue
+++ betterclassroom-frontend/src/views/ProfessorLoginView.vue
@@ -16,8 +16,8 @@ const password = ref('')
 const rawUrl = getApiUrl()
 const professorApiUrl = `http://${rawUrl}/api/professor`
 
-const register = async () => { 
-    router.push('/register')
+const register = async () => {
+  router.push('/register')
 }
 const login = async () => {
   const data = {
@@ -30,53 +30,72 @@ const login = async () => {
   try {
     //const response = await axios.post(`${professorApiUrl}/register`, data)
     user.value = {
-        email: email.value,
-        last_name: lastName.value,
-        first_name: firstName.value,
-        role: 'professor'
+      email: email.value,
+      last_name: lastName.value,
+      first_name: firstName.value,
+      role: 'professor'
     }
     router.push('/courses')
   } catch (e) {
     console.error(e)
   }
 }
+const changePassword = async () => {
+  router.push('/changePassword')
+}
 </script>
-<template>
-  <div class="flex flex-col h-full my-10 p-4 items-center justify-between border-4 border-black">
-    <h1>Login as a Professor</h1>
-    <label class="input input-bordered flex items-center gap-2 my-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        class="w-4 h-4 opacity-70"
-      >
-        <path
-          d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
-        />
-        <path
-          d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
-        />
-      </svg>
-      <input type="text" v-model="email" class="grow" placeholder="Email" />
-    </label>
-    <label class="input input-bordered flex items-center gap-2 my-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        class="w-4 h-4 opacity-70"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <input type="password" v-model="password" class="grow" value="password" />
-    </label>
 
-    <button @click="login" class="btn btn-primary">Anmelden</button>
-    <button @click="register" class="btn btn-primary">Noch keine Account?</button>
+<template>
+  <div class="flex flex-col items-center">
+    <div class="flex flex-col w-[400px] h-full my-10 p-8 justify-between shadow-2xl rounded-xl">
+      <h1 class="text-2xl">Better Classroom</h1>
+      <h1 class="text-xl font-bold mb-10">Professor Login</h1>
+      <label class="input input-bordered flex items-center gap-2 my-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          class="w-4 h-4 opacity-70 focus:ring-0 focus:border-transparent"
+        >
+          <path
+            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
+          />
+          <path
+            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
+          />
+        </svg>
+        <input type="text" v-model="email" class="grow" placeholder="Email" />
+      </label>
+      <label class="input input-bordered flex items-center gap-2 my-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          class="w-4 h-4 opacity-70"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <input type="password" v-model="password" class="grow" value="password" />
+      </label>
+
+      <button @click="login" class="btn btn-primary p-2 mt-3 mb-6">Anmelden</button>
+      <div class="flex flex-row justify-around">
+        <button @click="register" class="btn btn-secondary p-2 my-4">Noch keine Account?</button>
+        <button @click="changePassword" class="btn btn-secondary p-2 my-4">
+          Passwort vergessen?
+        </button>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+textarea:focus,
+input:focus {
+  outline: none;
+}
+</style>
