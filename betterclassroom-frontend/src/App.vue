c3fb72856f5d1b62a02b9be2d559e80b154ab2a4
Pascal Scheck <pascal.scheck@gmx.de>
2024-09-15 00:10:13 +0200

    toast styling

--- betterclassroom-frontend/src/App.vue
+++ betterclassroom-frontend/src/App.vue
@@ -1,25 +1,41 @@
 <script setup>
-import axios from 'axios'
-import { ref } from 'vue'
 import Navbar from './components/Navbar.vue'
 import Sidebar from './components/Sidebar.vue'
-
-const students = ref([])
-
-const fetchStudents = async () => {
-  try {
-    const response = await axios.get('http://better-classroom.com:8088/api/students')
-    console.log("Daten empfangen:", response.data);
-    students.value = response.data
-  } catch (error) {
-    console.error("Error fetching students:", error)
-  }
-}
 </script>
 
 <template>
   <Navbar />
   <Sidebar />
-  <notifications width="350px" position="top center" classes="my-notification" />
+  <notifications width="300px" position="top center" :duration="2000" :speed="300" classes="my-notification" />
 </template>
 
+<style>
+.my-notification {
+  margin: 80px 0;
+  padding: 10px;
+  font-size: 14px;
+  font-weight: 600;
+  border-radius: 0; /* Remove border radius to make it square */
+  opacity: 0.9;
+}
+
+.my-notification.success {
+  background-color: #4CAF50;
+  border-left: 5px solid #2E7D32;
+}
+
+.my-notification.error {
+  background-color: #F44336;
+  border-left: 5px solid #D32F2F;
+}
+
+.my-notification.warn {
+  background-color: #FFC107;
+  border-left: 5px solid #FFA000;
+}
+
+.my-notification.info {
+  background-color: #2196F3;
+  border-left: 5px solid #1976D2;
+}
+</style>
